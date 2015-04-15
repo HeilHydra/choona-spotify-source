ecd6bf1cb437f99224426226c35bb6477f74b85e
Oliver Woodings <oliver.woodings@qubitdigital.com>
2015-04-15 12:51:37 +0100

    Throttle streaming audio

--- service.js
+++ service.js
@@ -3,20 +3,23 @@ var SpotifyClient = require("./lib/SpotifyClient");
 var config = require("config");
 var lame = require("lame");
 var Speaker = require("speaker");
-var NRP = require("node-redis-pubsub");
+var Throttle = require("throttle");
 
 var app = new Waterway(config.waterway);
 var spotifyClient = new SpotifyClient(config.spotify);
 
-app.request("source", "spotify", "search")
-  .respond(function (data) {
-    return spotifyClient.search(data.search);
+app.request("source", "spotify", "search", ":searchString")
+  .respond(function (req) {
+    return spotifyClient.search(req.params.searchString);
   });
 
-app.stream("source", "spotify", "play")
-  .writable(function (stream, data, key) {
-    spotifyClient.get("spotify:track:" + data.id)
+app.stream("source", "spotify", "play", ":trackId")
+  .writable(function (stream, req) {
+    spotifyClient.get("spotify:track:" + req.params.trackId)
       .then(function (track) {
-        track.play().pipe(stream);
+        track
+          .play()
+          .pipe(new Throttle(20000))
+          .pipe(stream);
       });
   });
