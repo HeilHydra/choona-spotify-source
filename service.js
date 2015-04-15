8ae3ec5451c626e9641d997e58e4c9e5a2ea73bd
Oliver Woodings <oliver.woodings@qubitdigital.com>
2015-04-15 15:23:58 +0100

    Tweak throttle

--- service.js
+++ service.js
@@ -1,8 +1,6 @@
 var Waterway = require("waterway");
 var SpotifyClient = require("./lib/SpotifyClient");
 var config = require("config");
-var lame = require("lame");
-var Speaker = require("speaker");
 var Throttle = require("throttle");
 
 var app = new Waterway(config.waterway);
@@ -13,13 +11,34 @@ app.request("source", "spotify", "search", ":searchString")
     return spotifyClient.search(req.params.searchString);
   });
 
+app.request("source", "spotify", "getTrack", ":trackId")
+  .respond(function (req) {
+    return spotifyClient.get("spotify:track:" + req.params.trackId)
+      .then(function (track) {
+        normalizeTrack(track, req.params.trackId);
+        return track;
+      });
+  });
+
 app.stream("source", "spotify", "play", ":trackId")
   .writable(function (stream, req) {
     spotifyClient.get("spotify:track:" + req.params.trackId)
       .then(function (track) {
         track
-          .play()
-          .pipe(new Throttle(20000))
+          .playPreview()
+          .pipe(new Throttle(12000))
           .pipe(stream);
       });
   });
+
+
+function normalizeTrack(track, id) {
+  delete track.file;
+  delete track.preview;
+  delete track.alternative;
+  delete track._loaded;
+  track.id = id;
+  Object.defineProperty(track, "previewUrl", function () {
+    return "";
+  });
+}
